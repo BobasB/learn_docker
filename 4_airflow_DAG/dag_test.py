22984fe6d84852294b670ded1a5e473637d7a43e
BobasB <bbuhyl@cisco.com>
2020-06-23 13:40:00 +0300

    test new airflow dag with k8s operator

--- 4_airflow_DAG/dag_test.py
+++ 4_airflow_DAG/dag_test.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
 default_args = {
     'owner': 'airflow',
@@ -16,22 +17,41 @@ dag = DAG('helloWorld', schedule_interval='*/5 * * * *', default_args=default_ar
 t1 = BashOperator(
     task_id='task_1',
     bash_command='echo "Hello World from Task 1"',
-    dag=dag)
+    dag=dag
+)
 
-t2 = BashOperator(
+t2 = KubernetesPodOperator(
+    name='print-from-pod',
     task_id='task_2',
-    bash_command='echo "Hello World from Task 2"',
-    dag=dag)
+    namespace='testing',
+    service_account_name='default',
+    security_context={"fsGroup": 2000},
+    image='bobas/learn_docker:airflow',
+    image_pull_policy='Always',
+    resources={"request_cpu": "512m", "request_memory": "1Gi"},
+    cmds=["python", "-c"],
+    arguments=["from time import sleep; print(\"Simple K8S pod run\"); sleep(10)"],
+    env_vars={
+        'AWS_DEFAULT_REGION': "eu-west-1"
+    },
+    get_logs=True,
+    in_cluster=True,
+    is_delete_operator_pod=True,
+    labels={'internetEgress': 'allow'},
+    dag=dag
+)
 
 t3 = BashOperator(
     task_id='task_3',
     bash_command='echo "Hello World from Task 3"',
-    dag=dag)
+    dag=dag
+)
 
 t4 = BashOperator(
     task_id='task_4',
     bash_command='echo "Hello World from Task 4"',
-    dag=dag)
+    dag=dag
+)
 
 t2.set_upstream(t1)
 t3.set_upstream(t1)
