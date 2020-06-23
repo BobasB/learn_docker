75645ddd4ab3db75032b14f21dd8ed678edfedca
BobasB <bbuhyl@cisco.com>
2020-06-23 14:57:59 +0300

    added sleep to bash operators

--- 4_airflow_DAG/dag_test.py
+++ 4_airflow_DAG/dag_test.py
@@ -16,7 +16,7 @@ dag = DAG('helloWorld', schedule_interval='*/5 * * * *', default_args=default_ar
 
 t1 = BashOperator(
     task_id='task_1',
-    bash_command='echo "Hello World from Task 1"',
+    bash_command='echo "Hello World from Task 1"; sleep 30',
     dag=dag
 )
 
@@ -30,7 +30,7 @@ t2 = KubernetesPodOperator(
     image_pull_policy='Always',
     resources={"request_cpu": "512m", "request_memory": "1Gi"},
     cmds=["python", "-c"],
-    arguments=["from time import sleep; print(\"Simple K8S pod run\"); sleep(10)"],
+    arguments=["from time import sleep; print(\"Simple K8S pod run\"); sleep(30)"],
     env_vars={
         'AWS_DEFAULT_REGION': "eu-west-1"
     },
@@ -43,17 +43,16 @@ t2 = KubernetesPodOperator(
 
 t3 = BashOperator(
     task_id='task_3',
-    bash_command='echo "Hello World from Task 3"',
+    bash_command='echo "Hello World from Task 3"; sleep 30',
     dag=dag
 )
 
 t4 = BashOperator(
     task_id='task_4',
-    bash_command='echo "Hello World from Task 4"',
+    bash_command='echo "Hello World from Task 4"; sleep 30',
     dag=dag
 )
 
-t2.set_upstream(t1)
-t3.set_upstream(t1)
-t4.set_upstream(t2)
-t4.set_upstream(t3)
+t1 >> t2
+t1 >> t3 >> t2
+t1 >> t4
